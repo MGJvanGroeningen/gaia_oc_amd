27412d5de0ab937a9c5dd0ba3a943baf05ca6ea0
MGJvanGroeningen <matthijsvangroeningen@gmail.com>
2021-09-21 14:02:56 +0200

    test commit and push

---
+++ main.py
@@ -0,0 +1,7 @@
+# from file_with_model import Model
+#
+# data = load(data.csv)
+# train_data, test_data = split(data)
+# model = Model()
+# model.fit(train_data)
+# score = model.evaluate(test_data)
