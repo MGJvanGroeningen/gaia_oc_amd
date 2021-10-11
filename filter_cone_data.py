29b915ee7ce2445c6afee36f9eff67895648eedd
MGJvanGroeningen <matthijsvangroeningen@gmail.com>
2021-10-11 15:06:45 +0200

    fine tuning

--- filter_cone_data.py
+++ filter_cone_data.py
@@ -1,88 +1,6 @@
 import numpy as np
 
 
-def make_plx_candidate_rule(member_df, sigma_factor):
-    max_gmag = member_df['phot_g_mean_mag'].max()
-    min_gmag = member_df['phot_g_mean_mag'].min()
-    median_gmag = member_df['phot_g_mean_mag'].median()
-
-    low_gmag_member_df = member_df[(member_df['phot_g_mean_mag'] <= median_gmag)]
-    high_gmag_member_df = member_df[(member_df['phot_g_mean_mag'] > median_gmag)]
-
-    low_gmag_mean_plx = low_gmag_member_df['parallax'].mean()
-    high_gmag_mean_plx = high_gmag_member_df['parallax'].mean()
-
-    low_gmag_std_plx = low_gmag_member_df['parallax'].std()
-    high_gmag_std_plx = high_gmag_member_df['parallax'].std()
-
-    x1 = (median_gmag - min_gmag) / 2
-    x2 = (max_gmag - median_gmag) / 2
-
-    y1_top = low_gmag_mean_plx + sigma_factor * low_gmag_std_plx
-    y2_top = high_gmag_mean_plx + sigma_factor * high_gmag_std_plx
-
-    y1_bottom = low_gmag_mean_plx - sigma_factor * low_gmag_std_plx
-    y2_bottom = high_gmag_mean_plx - sigma_factor * high_gmag_std_plx
-
-    a_top = (y2_top - y1_top) / (x2 - x1)
-    b_top = y1_top - a_top * x1
-
-    a_bottom = (y2_bottom - y1_bottom) / (x2 - x1)
-    b_bottom = y1_bottom - a_bottom * x1
-
-    def candidate_rule(gmag, plx):
-        candidate = False
-        if min_gmag - 1 < gmag < max_gmag + 1 and \
-                a_top * plx + b_top > plx > a_bottom * plx + b_bottom:
-            candidate = True
-        return candidate
-
-    return candidate_rule
-
-
-def make_pm_candidate_rule(member_df, sigma_factor):
-    mean_pmra = member_df['pmra'].mean()
-    mean_pmdec = member_df['pmdec'].mean()
-
-    std_pmra = member_df['pmra'].std()
-    std_pmdec = member_df['pmdec'].std()
-
-    min_pmra = mean_pmra - sigma_factor * std_pmra
-    min_pmdec = mean_pmdec - sigma_factor * std_pmdec
-
-    max_pmra = mean_pmra + sigma_factor * std_pmra
-    max_pmdec = mean_pmdec + sigma_factor * std_pmdec
-
-    def candidate_rule(pmra, pmdec):
-        candidate = False
-        if min_pmra < pmra < max_pmra and min_pmdec < pmdec < max_pmdec:
-            candidate = True
-        return candidate
-
-    return candidate_rule
-
-
-def make_isochrone_candidate_rule(member_df, sigma_factor):
-    gmags = member_df['phot_g_mean_mag'].values
-    bp_rps = member_df['bp_rp'].values
-
-    members = np.array([bp_rps, gmags]).T
-
-    def candidate_rule(bp_rp, gmag):
-        point = np.array([bp_rp, gmag])
-        deltas = members - point
-        distances_squared = np.einsum('ij,ij->i', deltas, deltas)
-        closest_distance = np.sqrt(np.min(distances_squared))
-        # prob = np.sqrt(2 * np.pi) * np.mean(np.exp(- 0.5 * distances_squared))
-
-        candidate = False
-        if closest_distance < sigma_factor:
-            candidate = True
-        return candidate
-
-    return candidate_rule
-
-
 def distance_rule(df, max_distance, x_name, y_name):
     xs = df[x_name].values
     ys = df[y_name].values
@@ -103,14 +21,10 @@ def distance_rule(df, max_distance, x_name, y_name):
     return candidate_rule
 
 
-def make_candidate_rule(member_df, plx_sigma_factor, pm_sigma_factor, isochrone_sigma_factor):
-    # plx_candidate_rule = make_plx_candidate_rule(member_df, plx_sigma_factor)
-    # pm_candidate_rule = make_pm_candidate_rule(member_df, pm_sigma_factor)
-    # isochrone_candidate_rule = make_isochrone_candidate_rule(member_df, isochrone_sigma_factor)
-
-    plx_candidate_rule = distance_rule(member_df, plx_sigma_factor, 'phot_g_mean_mag', 'parallax')
-    pm_candidate_rule = distance_rule(member_df, pm_sigma_factor, 'pmra', 'pmdec')
-    isochrone_candidate_rule = distance_rule(member_df, isochrone_sigma_factor, 'bp_rp', 'phot_g_mean_mag')
+def make_candidate_rule(member_df, plx_max_d, pm_max_d, isochrone_max_d):
+    plx_candidate_rule = distance_rule(member_df, plx_max_d, 'phot_g_mean_mag', 'parallax')
+    pm_candidate_rule = distance_rule(member_df, pm_max_d, 'pmra', 'pmdec')
+    isochrone_candidate_rule = distance_rule(member_df, isochrone_max_d, 'bp_rp', 'phot_g_mean_mag')
 
     def candidate_rule(row):
         candidate = False
@@ -127,49 +41,53 @@ def divide_cone(cone_df,
                 cluster_df,
                 train_columns,
                 probability_threshold,
-                plx_sigma_factor,
-                pm_sigma_factor,
-                isochrone_sigma_factor,
-                drop_database_candidates=False):
-    print(f'Total number of sources in cone: {len(cone_df)}')
+                plx_max_d,
+                pm_max_d,
+                isochrone_max_d,
+                drop_db_candidates=False):
+    n_sources = len(cone_df)
+    print(f'Total number of sources in cone: {n_sources}')
 
-    # make a new column in the cone dataframe that indicates whether a source belongs to the cluster
-    member_source_ids = cluster_df[(cluster_df['PMemb'].astype(np.float32) >=
-                                    probability_threshold)]['Source'].astype(np.int64).values
-    member_df = cone_df[(cone_df['source_id'].isin(member_source_ids))][train_columns].dropna()
+    # select high probability members from the cone
+    high_prob_member_source_ids = cluster_df[(cluster_df['PMemb'].astype(np.float32) >= 
+                                              probability_threshold)]['Source'].astype(np.int64).values
+    high_prob_member_df = cone_df[(cone_df['source_id'].isin(high_prob_member_source_ids))][train_columns].dropna()
+    n_high_prob_member = len(high_prob_member_df)
+    print(f'{n_high_prob_member} cluster sources were selected with a membership probability of >= '
+          f'{probability_threshold}')
+    
+    # select low probability members from the cone
+    low_prob_member_source_ids = cluster_df[(cluster_df['PMemb'].astype(np.float32) <
+                                             probability_threshold)]['Source'].astype(np.int64).values
+    low_prob_member_df = cone_df[(cone_df['source_id'].isin(low_prob_member_source_ids))][train_columns].dropna()
+    n_low_prob_members = len(low_prob_member_df)
+    print(f'{n_low_prob_members} cluster sources had a too low probability to be used for the training set')
+    
+    # drop high probability members from the cone to obtain the field
+    field_df = cone_df.drop(high_prob_member_df.index)[train_columns].dropna()
+    n_field = len(field_df)
+    n_incomplete_rows = n_sources - n_field - n_high_prob_member
+    print(f'{n_incomplete_rows} sources were removed from the cone because of incomplete data')
 
-    print(f'{len(member_df)} cluster sources were selected with a membership probability of >= {probability_threshold}')
+    # optionally prevent the low probability members to be selected as candidates
+    if drop_db_candidates:
+        suffix = '(low database probability cluster sources were excluded)'
+        field_df = field_df.drop(low_prob_member_df.index)
+    else:
+        suffix = '(may include low database probability cluster sources)'
+    
+    # select member candidates based on distance from high probability members in plx, pm and isochrone space
+    candidate_rule = make_candidate_rule(high_prob_member_df, plx_max_d, pm_max_d, isochrone_max_d)
+    candidate_df = field_df[field_df.apply(candidate_rule, axis=1)]
+    n_candidates = len(candidate_df)
+    print(f'{n_candidates} sources were selected as candidates {suffix}')
 
-    field_df = cone_df.drop(member_df.index)[train_columns].dropna()
-
-    database_candidate_source_ids = cluster_df[(cluster_df['PMemb'].astype(np.float32) <
-                                                probability_threshold)]['Source'].astype(np.int64).values
-    database_candidate_df = cone_df[(cone_df['source_id'].isin(database_candidate_source_ids))][train_columns].dropna()
-
-    print(f'{len(database_candidate_df)} cluster sources had a too low probability to be used for the training set')
-
-    if drop_database_candidates:
-        field_df = field_df.drop(database_candidate_df.index)
-
-    candidate_rule = make_candidate_rule(member_df,
-                                         plx_sigma_factor,
-                                         pm_sigma_factor,
-                                         isochrone_sigma_factor)
-
-    field_df['candidate'] = field_df.apply(candidate_rule, axis=1)
-
-    candidate_df = field_df[(field_df['candidate'])]
-
-    print(f'{len(candidate_df)} sources were selected as candidates')
-
-    candidate_df = candidate_df.drop('candidate', axis=1)
-    field_df = field_df.drop('candidate', axis=1)
-
+    # select non members by removing the candidates from the field
     if len(candidate_df) > 0:
         non_member_df = field_df.append(candidate_df).drop_duplicates(keep=False)
     else:
         non_member_df = field_df
+    n_non_members = len(non_member_df)
+    print(f'{n_non_members} sources were selected as non members')
 
-    print(f'{len(non_member_df)} sources were selected as non members')
-
-    return member_df, database_candidate_df, candidate_df, non_member_df
+    return high_prob_member_df, low_prob_member_df, candidate_df, non_member_df
--- rf_cone_data_advanced_practice.py
+++ rf_cone_data_advanced_practice.py
@@ -6,18 +6,29 @@ from sklearn.ensemble import RandomForestClassifier
 from filter_cone_data import divide_cone
 
 
-def extinction_correction(isochrones, member_df, extinction_v):
+def isochrone_correction(isochrones, distance, extinction_v):
     extinction_g = 0.83627 * extinction_v
-    mean_plx = np.mean(member_df['parallax'].values)  # in mas
-    mean_d = 1000 / mean_plx
-    gmag_correction = 5 * np.log10(mean_d) - 5 - extinction_g
-    bp_rp_correction = (1.08337 - 0.6343) * extinction_v
+    extinction_bp = 1.08337 * extinction_v
+    extinction_rp = 0.6343 * extinction_v
+
+    distance_modulus = 5 * np.log10(distance) - 5
+
+    gmag_correction = distance_modulus + extinction_g
+    bpmag_correction = distance_modulus + extinction_bp
+    rpmag_correction = distance_modulus + extinction_rp
 
     for isochrone in isochrones:
-        isochrone['bp_rp'] = isochrone['G_BPmag'] - isochrone['G_RPmag'] + bp_rp_correction
         isochrone['Gmag'] += gmag_correction
+        isochrone['G_BPmag'] += bpmag_correction
+        isochrone['G_RPmag'] += rpmag_correction
+        isochrone['bp_rp'] = isochrone['G_BPmag'] - isochrone['G_RPmag']
 
     return isochrones
+
+
+def duplicates(df1, df2):
+    combined = df1.append(df2)
+    return combined[combined.duplicated(keep='last')]
 
 
 def rf_advanced_practice_with_cone(data_dir,
@@ -26,11 +37,11 @@ def rf_advanced_practice_with_cone(data_dir,
                                    isochrone_file,
                                    probability_threshold,
                                    train_columns,
-                                   plx_sigma_factor=1.0,
-                                   pm_sigma_factor=1.0,
-                                   isochrone_sigma_factor=0.3,
+                                   plx_max_d=1.0,
+                                   pm_max_d=1.0,
+                                   isochrone_max_d=1.0,
                                    cluster_name='cluster',
-                                   drop_database_candidates=False,
+                                   drop_db_candidates=False,
                                    plot_predicted_members=True,
                                    plot_predicted_non_members=True):
     # load practice data
@@ -47,125 +58,118 @@ def rf_advanced_practice_with_cone(data_dir,
     cluster_df = cluster_df.iloc[2:]
     cluster_df = cluster_df.reset_index(drop=True)
 
-    # create isochrone dataframe
-    isochrone_df = pd.read_csv(practice_data_isochrone_file, sep='\s+', comment='#')
+    # create isochrones dataframe
+    isochrones_df = pd.read_csv(practice_data_isochrone_file, delim_whitespace=True, comment='#')
 
+    # divide isochrones dataframe in a separate dataframe for each isochrone
     isochrones = []
-    for age in list(set(isochrone_df['logAge'].values)):
-        isochrones.append(isochrone_df[(isochrone_df['logAge'] == age)].iloc[:135])
+    for age in list(set(isochrones_df['logAge'].values)):
+        isochrones.append(isochrones_df[(isochrones_df['logAge'] == age)].iloc[:135])
 
-    members, database_candidates, candidates, non_members = divide_cone(cone_df,
-                                                                        cluster_df,
-                                                                        train_columns,
-                                                                        probability_threshold,
-                                                                        plx_sigma_factor,
-                                                                        pm_sigma_factor,
-                                                                        isochrone_sigma_factor,
-                                                                        drop_database_candidates=drop_database_candidates)
+    # divide the cone in high probability members, low probability members, candidates and non members
+    high_prob_members, low_prob_members, candidates, non_members = divide_cone(cone_df,
+                                                                               cluster_df,
+                                                                               train_columns,
+                                                                               probability_threshold,
+                                                                               plx_max_d,
+                                                                               pm_max_d,
+                                                                               isochrone_max_d,
+                                                                               drop_db_candidates=drop_db_candidates)
 
     if len(candidates) > 0:
-        alpha_non_members = 0.8
-        dot_size = 3.0
-        isochrones = extinction_correction(isochrones, members, extinction_v=0.15)
+        # correct the isochrones for distance and extinction
+        mean_plx = np.mean(high_prob_members['parallax'].values)  # in mas
+        mean_distance = 1000 / mean_plx
+
+        # cheating with distance and extinction to make isochrone fit
+        isochrones = isochrone_correction(isochrones, 0.80 * mean_distance, extinction_v=0.05)
 
         # create training data for random forest
-        train_data = pd.concat([members, non_members], ignore_index=True)
+        train_data = pd.concat([high_prob_members, non_members], ignore_index=True)
         x_train = train_data.values
-        y_train = np.concatenate((np.ones(len(members)), np.zeros(len(non_members))))
+        y_train = np.concatenate((np.ones(len(high_prob_members)), np.zeros(len(non_members))))
         n_train = x_train.shape[0]
         print(f'\nTraining set size: {n_train}')
 
         # create test data for random forest
-        test_data = candidates
-        x_test = test_data.values
+        x_test = candidates.values
         n_test = x_test.shape[0]
         print(f'Test set size: {n_test}')
 
+        # train random forest with high probability members and non members
         max_depth = 10
-
         clf = RandomForestClassifier(max_depth=max_depth, random_state=0)
         clf.fit(x_train, y_train)
 
-        predictions = clf.predict(x_test)
-        candidates['pred'] = predictions
+        # predict membership of candidates
+        predictions = clf.predict(x_test).astype(bool)
 
-        predicted_members = candidates[(candidates['pred'] == 1)]
-        predicted_non_members = candidates[(candidates['pred'] == 0)]
-
-        n_duplicates = sum(database_candidates.append(predicted_members.drop('pred', axis=1)).duplicated().values.astype(np.int32))
+        # make subsets from candidates based on membership prediction
+        predicted_members = candidates[predictions]
+        predicted_non_members = candidates[np.invert(predictions)]
 
         print(f'\n{len(predicted_members)}/{len(candidates)} of the candidates were predicted to be members')
-        print(f'{n_duplicates}/{len(database_candidates)} predicted members are also present in the database candidates')
         print(f'{len(predicted_non_members)}/{len(candidates)} of the candidates were predicted to be non members')
 
-        fig, ax = plt.subplots(2, 2, figsize=(12, 12))
-        ax[0, 0].scatter(non_members['ra'], non_members['DEC'],
-                         label='non members', s=0.1, c='gray', alpha=alpha_non_members)
-        ax[0, 0].scatter(members['ra'], members['DEC'],
-                         label=f'members (>= {probability_threshold})', s=dot_size, c='red')
-        if plot_predicted_non_members:
-            ax[0, 0].scatter(predicted_non_members['ra'], predicted_non_members['DEC'],
-                             label=f'predicted_non_members', s=dot_size, c='purple')
-        if plot_predicted_members:
-            ax[0, 0].scatter(predicted_members['ra'], predicted_members['DEC'],
-                             label=f'predicted_members', s=dot_size, c='blue')
-        ax[0, 0].set_title(cluster_name)
-        ax[0, 0].set_xlabel('RA')
-        ax[0, 0].set_ylabel('DEC')
-        ax[0, 0].legend()
+        # see how the low probability members were treated by the random forest
+        if not drop_db_candidates:
+            low_prob_members_in_predicted_members = duplicates(low_prob_members, predicted_members)
+            low_prob_members_in_predicted_non_members = duplicates(low_prob_members, predicted_non_members)
+            low_prob_members_in_field = duplicates(low_prob_members, non_members)
 
-        ax[1, 0].scatter(non_members['bp_rp'], non_members['phot_g_mean_mag'],
-                         label='non members', s=0.1, c='gray', alpha=alpha_non_members)
-        ax[1, 0].scatter(members['bp_rp'], members['phot_g_mean_mag'],
-                         label=f'members (>= {probability_threshold})', s=dot_size, c='red')
-        if plot_predicted_non_members:
-            ax[1, 0].scatter(predicted_non_members['bp_rp'], predicted_non_members['phot_g_mean_mag'],
-                             label=f'predicted_non_members', s=dot_size, c='purple')
-        if plot_predicted_members:
-            ax[1, 0].scatter(predicted_members['bp_rp'], predicted_members['phot_g_mean_mag'],
-                             label=f'predicted_members', s=dot_size, c='blue')
-        for isochrone in [isochrones[4]]:
+            n_low_prob_members_in_predicted_members = len(low_prob_members_in_predicted_members)
+            n_low_prob_members_in_predicted_non_members = len(low_prob_members_in_predicted_non_members)
+            n_low_prob_members_in_field = len(low_prob_members_in_field)
+            n_low_prob_members_candidates = len(low_prob_members)
+
+            print(f'\n{n_low_prob_members_in_predicted_members}/{n_low_prob_members_candidates} '
+                  f'of low probability members were predicted to be members')
+            print(f'{n_low_prob_members_in_predicted_non_members}/{n_low_prob_members_candidates} '
+                  f'of low probability members were predicted to be non members')
+            print(f'{n_low_prob_members_in_field}/{n_low_prob_members_candidates} '
+                  f'of low probability members were not selected as candidates')
+
+        # plot results
+        fig, ax = plt.subplots(2, 2, figsize=(16, 16))
+        alpha_non_members = 0.8
+        dot_size = 3.0
+
+        def plot_func(axis, x, y):
+            axis.scatter(non_members[x], non_members[y], label='non members', s=0.1 * dot_size, c='gray',
+                         alpha=alpha_non_members)
+            if plot_predicted_non_members:
+                axis.scatter(predicted_non_members[x], predicted_non_members[y], label=f'predicted_non_members',
+                             s=dot_size, c='gray')
+                axis.scatter(low_prob_members_in_predicted_non_members[x], low_prob_members_in_predicted_non_members[y],
+                             marker='+', label=f'predicted_non_members_in_db', s=10 * dot_size, c='gray')
+            axis.scatter(high_prob_members[x], high_prob_members[y], label=f'members (>= {probability_threshold})',
+                         s=dot_size, c='red')
+            if plot_predicted_members:
+                axis.scatter(predicted_members[x], predicted_members[y], label=f'predicted_members', s=dot_size,
+                             c='blue')
+                axis.scatter(low_prob_members_in_predicted_members[x], low_prob_members_in_predicted_members[y],
+                             marker='+', label=f'predicted_members_in_db', s=10 * dot_size, c='blue')
+            axis.set_title(cluster_name)
+            axis.set_xlabel(x)
+            axis.set_ylabel(y)
+            axis.legend(fontsize='small')
+
+        plot_func(ax[0, 0], 'ra', 'DEC')
+
+        plot_func(ax[1, 0], 'bp_rp', 'phot_g_mean_mag')
+        for isochrone in [isochrones[5]]:
             ax[1, 0].plot(isochrone['bp_rp'], isochrone['Gmag'])  # , label=f'{isochrone["logAge"].values[0]}')
-        ax[1, 0].set_title(cluster_name)
         ax[1, 0].set_xlim(-0.5, 2.5)
         ax[1, 0].set_ylim(10, 20)
         ax[1, 0].invert_yaxis()
-        ax[1, 0].set_xlabel('bp_rp')
-        ax[1, 0].set_ylabel('phot_g_mean_mag')
-        ax[1, 0].legend()
 
-        ax[0, 1].scatter(non_members['pmra'], non_members['pmdec'],
-                         label='non members', s=0.1, c='gray', alpha=alpha_non_members)
-        ax[0, 1].scatter(members['pmra'], members['pmdec'],
-                         label=f'members (>= {probability_threshold})', s=dot_size, c='red')
-        if plot_predicted_non_members:
-            ax[0, 1].scatter(predicted_non_members['pmra'], predicted_non_members['pmdec'],
-                             label=f'predicted_non_members', s=dot_size, c='purple')
-        if plot_predicted_members:
-            ax[0, 1].scatter(predicted_members['pmra'], predicted_members['pmdec'],
-                             label=f'predicted_members', s=dot_size, c='blue')
-        ax[0, 1].set_title(cluster_name)
-        ax[0, 1].set_xlabel('pmra')
-        ax[0, 1].set_ylabel('pmdec')
-        ax[0, 1].set_xlim(-4.6, -0.6)
-        ax[0, 1].set_ylim(-1.2, 2.8)
-        ax[0, 1].legend()
+        plot_func(ax[0, 1], 'pmra', 'pmdec')
+        ax[0, 1].set_xlim(-3.6, -1.6)
+        ax[0, 1].set_ylim(-0.2, 1.8)
 
-        ax[1, 1].scatter(non_members['phot_g_mean_mag'], non_members['parallax'],
-                         label='non members', s=0.1, c='gray', alpha=alpha_non_members)
-        ax[1, 1].scatter(members['phot_g_mean_mag'], members['parallax'],
-                         label=f'members (>= {probability_threshold})', s=dot_size, c='red')
-        if plot_predicted_non_members:
-            ax[1, 1].scatter(predicted_non_members['phot_g_mean_mag'], predicted_non_members['parallax'],
-                             label=f'predicted_non_members', s=dot_size, c='purple')
-        if plot_predicted_members:
-            ax[1, 1].scatter(predicted_members['phot_g_mean_mag'], predicted_members['parallax'],
-                             label=f'predicted_members', s=dot_size, c='blue')
-        ax[1, 1].set_title(cluster_name)
-        ax[1, 1].set_xlabel('phot_g_mean_mag')
-        ax[1, 1].set_ylabel('parallax')
-        ax[1, 1].set_ylim(-1.0, 1.0)
-        ax[1, 1].legend()
+        plot_func(ax[1, 1], 'phot_g_mean_mag', 'parallax')
+        ax[1, 1].set_ylim(-0.5, 1.0)
+        plt.savefig('results/NGC_2509_plots.pdf')
         plt.show()
     else:
         print('No candidates were found!')
@@ -173,10 +177,10 @@ def rf_advanced_practice_with_cone(data_dir,
 
 if __name__ == "__main__":
     # lists of columns to use for training the random forest
-    tgas_cone_columns = ['ra', 'DEC', 'parallax', 'pmra', 'pmdec']
-    tgas_and_photometric_cone_columns = ['ra', 'DEC', 'parallax', 'pmra', 'pmdec', 'phot_g_mean_mag', 'bp_rp', 'ruwe']
+    small_set = ['ra', 'DEC', 'parallax', 'pmra', 'pmdec']
+    big_set = ['ra', 'DEC', 'parallax', 'pmra', 'pmdec', 'phot_g_mean_mag', 'bp_rp', 'ruwe']
 
-    train_col = tgas_and_photometric_cone_columns
+    train_col = big_set
 
     rf_advanced_practice_with_cone(data_dir='practice_data',
                                    cone_file='NGC_2509_cone.csv',
@@ -184,10 +188,10 @@ if __name__ == "__main__":
                                    isochrone_file='isochrone.dat',
                                    probability_threshold=0.9,
                                    train_columns=train_col,
-                                   plx_sigma_factor=0.3,
-                                   pm_sigma_factor=1.0,
-                                   isochrone_sigma_factor=0.4,
+                                   plx_max_d=0.7,
+                                   pm_max_d=0.9,
+                                   isochrone_max_d=0.9,
                                    cluster_name='NCG_2509',
-                                   drop_database_candidates=False,
+                                   drop_db_candidates=False,
                                    plot_predicted_members=True,
-                                   plot_predicted_non_members=False)
+                                   plot_predicted_non_members=True)
